75c579b18dcbc0e28f1a9c764e5730fdfb84d879
yashdarekar17 <dyash8608@gmail.com>
2024-10-30 15:26:18 -0200

    restore it

---
+++ education.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    Strings:["Web development","App development","UI/UX","Data science"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
