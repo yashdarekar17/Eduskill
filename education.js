3531474943cd2c9b82d787b02ac07d2c1154827b
yashdarekar17 <dyash8608@gmail.com>
2024-12-24 18:32:43 -0200

    Initial commit

--- education.js
+++ education.js
@@ -1,7 +1,20 @@
-var typed = new Typed(".text",{
-    Strings:["Web development","App development","UI/UX","Data science"],
+
+// var typed = new Typed(".text",{
+//     Strings:["Web development","App development","UI/UX","Data science"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// })
+
+
+
+var typed =new Typed(".text" , {
+    strings: ["Web development","App development","UI/UX","Data science"], // Your custom strings here
     typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
+     backSpeed:100,
+      backDelay:1000,
     loop:true
-})
+});
+
+var typed = new Typed("#typed-output", options);
